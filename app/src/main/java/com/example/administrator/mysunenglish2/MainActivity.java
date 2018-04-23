f9eb37cd41bedbb17d409072733c3a2bf4c00d9c
Administrator <6453776@qq.com>
2018-04-23 18:38:12 +0800

    0423.001

--- app/src/main/java/com/example/administrator/mysunenglish2/MainActivity.java
+++ app/src/main/java/com/example/administrator/mysunenglish2/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 //2018年4月19日完成测试
 //
 
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //检测有没有打开左侧的菜单如打开先关了 左菜单.  没有打开直接退出
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -72,6 +74,17 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "action_settings", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.action_open) {
+            Toast.makeText(this, "action_open", Toast.LENGTH_SHORT).show();
+            this.openWebSite("http://oa.9oe.com/index.php/scrapy");
+            return true;
+        }
+        if (id == R.id.action_close) {
+            Toast.makeText(this, "action_close", Toast.LENGTH_SHORT).show();
+            this.openWebSite("http://oa.9oe.com/index.php/blog");
             return true;
         }
 
@@ -109,6 +122,56 @@ public class MainActivity extends AppCompatActivity
 //        webView.loadUrl("file:///android_asset/example.html");
         //WebView加载web资源
         webView.loadUrl("https://m.baidu.com");
+        //覆盖WebView默认通过第三方或者是系统浏览器打开网页的行为，使得网页可以在WebView中打开
+
+        //启用支持Javascript
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        //WebView加载页面优先使用缓存加载
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        //页面加载
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                //newProgress   1-100之间的整数
+                if (newProgress == 100) {
+                    //页面加载完成，关闭ProgressDialog
+                    closeDialog();
+                } else {
+                    //网页正在加载，打开ProgressDialog
+                    openDialog(newProgress);
+                }
+            }
+
+            private void openDialog(int newProgress) {
+
+                if (dialog == null) {
+                    dialog = new ProgressDialog(MainActivity.this);
+                    dialog.setTitle("正在加载");
+                    dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                    dialog.setProgress(newProgress);
+                    dialog.setCancelable(true);
+                    dialog.show();
+                } else {
+                    dialog.setProgress(newProgress);
+                }
+            }
+
+            private void closeDialog() {
+                if (dialog != null && dialog.isShowing()) {
+                    dialog.dismiss();
+                    dialog = null;
+                }
+            }
+        });
+    }
+
+    private void openWebSite(String strUrl) {
+        webView = (WebView) findViewById(R.id.webView);
+        //WebView加载本地资源
+//        webView.loadUrl("file:///android_asset/example.html");
+        //WebView加载web资源
+        webView.loadUrl(strUrl);
         //覆盖WebView默认通过第三方或者是系统浏览器打开网页的行为，使得网页可以在WebView中打开
 
         //启用支持Javascript
